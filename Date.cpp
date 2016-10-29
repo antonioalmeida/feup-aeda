28bb9c9fd8980fa6e643a1f9a58b09b94c46b26a
Cyril Damas <jpdamas15@gmail.com>
2016-10-29 20:30:24 +0100

    Partial implementation of class Date (exception handling missing)

--- Date.cpp
+++ Date.cpp
@@ -1,3 +1,44 @@
+#include <sstream>
 #include "Date.h"
+#include "Exceptions.h"
 
-//Source file of class Date (to be implemented)
+using namespace std;
+
+Date::Date(std::string date) {
+	istringstream dateTemp;
+	dateTemp.str(date);
+	dateTemp >> day;
+	dateTemp.ignore();
+	dateTemp >> month;
+	dateTemp.ignore();
+	dateTemp >> year;
+	/*To add: Exception handling (if day is bigger than 31, month bigger than 12 or date is not valid according to Gregorian calendar standars; exception classes need to be created*/
+}
+
+Date::Date(unsigned int day, unsigned int month, unsigned int year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+	/*To add: Exception handling (if day is bigger than 31, month bigger than 12 or date is not valid according to Gregorian calendar standars; exception classes need to be created*/
+}
+
+unsigned int Date::getDay() const {
+	return day;
+}
+
+unsigned int Date::getMonth() const {
+	return month;
+}
+
+unsigned int Date::getYear() const {
+	return year;
+}
+
+ostream& operator<<(ostream& out, const Date &date) {
+	out << date.getDay() << "/" << date.getMonth() << "/" << date.getYear();
+	return out;
+}
+
+bool operator<(const Date &date1, const Date &date2) {
+	return (date1.year < date2.year) || (date1.year == date2.year && date1.month < date2.month) || (date1.year == date2.year && date1.month == date2.month && date1.day < date2.day);
+}
--- Date.h
+++ Date.h
@@ -1,3 +1,27 @@
 #pragma once
 
-//Header file for class Date (to be implemented)
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Date {
+private:
+	unsigned int day;
+	unsigned int month;
+	unsigned int year;
+public:
+	Date(){}; //Default constructor (does nothing; just here because since there are other constructors, default one isn't automatically created)
+	Date(std::string date); //Date is given in a string in the format DD/MM/YYYY (to use when reading information from files)
+	Date(unsigned int day, unsigned int month, unsigned int year); //Date elements given in separate (to use with run-time created objects)
+	unsigned int getDay() const; //Returns date day
+	unsigned int getMonth() const; //Returns date month
+	unsigned int getYear() const; //Returns date year
+	/* Not sure if these will be useful yet
+	void setDay(unsigned int newDay); //Sets date's day to newDay
+	void setMonth(unsigned int newMonth); //Sets date's month to newMonth
+	void setYear(unsigned int newYear); //Sets date's year to newYear
+	*/
+
+	friend ostream& operator<<(ostream& out, const Date &date); //Overload of << operator so it can be send to cout like objects
+	friend bool operator<(const Date &date1, const Date &date2); //Overload of < operator to compare two dates
+};
