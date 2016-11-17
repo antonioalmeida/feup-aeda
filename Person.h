3d2f5e779567ef3f3e7330504611b2f4f0f634f0
Cyril Damas <jpdamas15@gmail.com>
2016-11-17 18:08:43 +0000

    [Person] Finished Doxygen comment style
    Doxygen comment style is now added where necessary in Person.h

--- Person.h
+++ Person.h
@@ -13,7 +13,7 @@
 class Person {
 protected:
 	std::string name;
-	std::string email; //created directly from code ("up"+code+"@fe.up.pt" for Student, code+"@fe.up.pt" for Teacher)
+	std::string email;
 public:
     //! Default constructor
 	Person() {};
@@ -62,17 +62,17 @@ class Student : public Person {
 protected:
 	unsigned long int code;
 	std::string status;
-	float ectsTaking; //Automatically 0 when object is created
+	float ectsTaking;
 	unsigned int curricularYear;
-	double average; //Automatically 0.0 for freshmen, calculated after unitsDone and unitsTodo have all the information otherwise
-	std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsDone; //When reading from file, curricularYear-1 iterations will be done, each line corresponding to the grades to subjects of a curricular year (only save the ones where grade >= 10!)
-	std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsToDo; //Same as above but only save units with final grade < 10 (grade saved because it also counts for the average; to use later in the registration process to guarantee units left behind are picked first)
-	std::vector<Unit*> unitsTaking; //Units chosen during registration (+ units left behind - do not add automatically, user must do it himself)
-	bool registrationComplete; //Automatically false when object is created, true after student is registered
-	Date registrationDate; //Not sure how it'll be handled, but needs to be here since it's stated in the paper...
+	double average;
+	std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsDone;
+	std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsToDo;
+	std::vector<Unit*> unitsTaking;
+	bool registrationComplete;
+	Date registrationDate;
 
 public:
-    //! Sequential code used for 1st timers (unsigned int may not have enough memory, unsigned long should hold easily)
+    //! Sequential code used for freshmen
 	static unsigned long int nextCode;
     
     //! Default constructor
@@ -91,31 +91,118 @@ public:
      \param curricularYear unsigned int that holds the student's curricylar year
      \param unitsDone vector that holds the student's completed units
      \param unitsToDo vector that hold the student's uncompleted unites
-     \param code int that holds the students code (automatically created if curricularYear == 1)
+     \param code int that holds the students code (automatically assigned if curricularYear is 1)
      */
 	Student(std::string name, std::string status, unsigned int curricularYear, std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsDone, std::vector<std::vector<std::pair<Unit*, unsigned int>>> unitsToDo, unsigned long int code = nextCode++);
 
+	//! Gets a copy of the student's unique code
+	/*!
+	\return copy of unsigned long it data member code
+	*/
 	unsigned long int getCode() const;
+	
+	//! Gets a copy of the student's status
+	/*!
+	\return copy of string data member status
+	*/
 	std::string getStatus() const;
+	
+	//! Gets a copy of the amount of ECTS the student is currently taking
+	/*!
+	\return copy of float data member ectsTaking
+	*/
 	float getECTSTaking() const;
+	
+	//! Gets a copy of the student's current curricular year
+	/*!
+	\return copy of unsigned int data member curricularYear
+	*/
 	unsigned int getCurricularYear() const;
+	
+	//! Gets a copy of the value of the student's current average
+	/*!
+	\return copy of float data member average
+	*/
 	double getAverage() const;
+	
+	//! Gets a copy of the matrix that holds the information about units the student has taken and completed (pairs consisting of a pointer to the unit and the final grade)
+	/*!
+	\return copy of data member pair vector vectors of type <Unit*, unsigned int> unitsDone
+	*/
 	std::vector<std::vector<std::pair<Unit*, unsigned int>>> getUnitsDone() const;
+	
+	//! Gets a copy of the matrix that holds the information about units the student has taken but not completed (pairs consisting of a pointer to the unit and the final grade)
+	/*!
+	\return copy of data member pair vector vectors of type <Unit*, unsigned int> unitsToDo
+	*/
 	std::vector<std::vector<std::pair<Unit*, unsigned int>>> getUnitsToDo() const;
+	
+	//! Gets a copy of the vector that holds pointers to the units the student's taking
+	/*!
+	\return copy of data member vector of type Unit* unitsTaking
+	*/
 	std::vector<Unit*> getUnitsTaking() const;
-	Date getRegistrationDate() const; //If registrationComplete = false, do what?
+	
+	//! Gets a copy of the student's registration date
+	/*!
+	\return copy of data member registrationDate, an object of custom class Date
+	*/
+	Date getRegistrationDate() const;
 
+	//! Adds a pair consisting of a unit the student has taken and the (positive) grade he obtained at it to the unitsDone matrix
+	/*!
+	\param p pair whose left side is a pointer to the unit and the right side is the final grade obtained
+	*/
 	void addUnitDone(std::pair<Unit*, unsigned int> p);
+
+	//! Adds a pair consisting of a unit the student has taken and the (negative) grade he obtained at it to the unitsToDo matrix
+	/*!
+	\param p pair whose left side is a pointer to the unit and the right side is the final grade obtained
+	*/
 	void addUnitToDo(std::pair<Unit*, unsigned int> p);
+	
+	//! Adds a unit that the student is taking to the unitsTaking vector. Throws exception if adding the unit would cause an ECTS overflow
+	/*!
+	\param u pointer to the unit to be added
+	*/
 	void addUnitTaking(Unit* u);
+
+	//! Calculates the student's current average. Is left at 0 if it's a freshman
 	void calculateAverage();
+
+	//! Checks if given student has completed the registration process
+	/*!
+	\return current value of data member isRegistered: true if student has completed registration, false otherwise
+	*/
 	bool isRegistered() const; //Check if student is already registered, if so he'll be ignored in the registration process
 	
-	void setRegistration(); //The only state possible is to go from false to true so keeping it simple
+	//! Changes a student's registration status to its current opposite (true if false and vice-versa)
+	void setRegistration();
+	
+	//! Changes a student's status to a new one
+	/*!
+	\param newStatus string corresponding to the student's new status
+	*/
 	void setStatus(std::string newStatus);
 	
+	//! Prints student to out (used when writing to file)
+	/*!
+	\param out outstream where the student will be printed to
+	*/
 	virtual void save(std::ostream &out) const;
-	bool operator==(const Student &s1); //!< Students are equal if they have the same code (= same person)
+
+	//! Overload of operator== to test equalty between students
+	/*!
+	\param s student that it will be compared to
+	\return true if students have the same code, false otherwise
+	*/
+	bool operator==(const Student &s1);
+
+	//! Overload of operator<< to print students to screen
+	/*!
+	\param out outstream where the student will be printed to
+	\param s student that will be printed
+	*/
 	friend std::ostream& operator<<(std::ostream& out, const Student& s);
 
 };
@@ -206,8 +293,34 @@ public:
 	friend std::ostream& operator<<(std::ostream& out, const Teacher& s);
 };
 
-bool sortByName(Student s1, Student s2); //!< Returns true if s1.name < s2.name
-bool sortByAverage(Student s1, Student s2); //!< Returns true if s1.average < s2.average
+//! Compares two students by their names (used as custom comparison function in STL sort)
+/*!
+\param s1 first student of the two to be compared
+\param s2 second student of the two to be compared
+\return true if student s1's name is alphabetically inferior to student s2's name, false otherwise
+*/
+bool sortByName(Student s1, Student s2);
+
+//! Compares two students by their current course average (used as custom comparison function in STL sort)
+/*!
+\param s1 first student of the two to be compared
+\param s2 second student of the two to be compared
+\return true if s1's average is inferior to s2's average, false otherwise
+*/
+bool sortByAverage(Student s1, Student s2);
+
+//! Compares two students by their current curricular year (used as custom comparison function in STL sort)
+/*!
+\param s1 first student of the two to be compared
+\param s2 second student of the two to be compared
+\return true if s1's curricular year is inferior to s2's curricular year, false otherwise
+*/
 bool sortByCurricularYear(Student s1, Student s2); //!< Returns true if s1.curricularYear < s2.curricularYear
 
-bool sortTByName(Teacher t1, Teacher t2); //!< Returns true if t1.name < t2.name
+//! Compares two teachers by their names (used as custom comparison function in STL sort)
+/*!
+\param t1 first teacher of the two to be compared
+\param t2 second teacher of the two to be compared
+\return true if t1's name is alphabetically inferior to t2's name, false otherwise
+*/
+bool sortTByName(Teacher t1, Teacher t2);
